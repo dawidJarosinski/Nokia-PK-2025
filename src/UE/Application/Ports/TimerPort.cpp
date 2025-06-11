be0b3d2971863f0970dc5c254dea1c67a19c73dc
Kamil Glazer <k.glazer.kamil@interia.pl>
2025-06-11 22:00:44 +0200

    Modified ports for calling

--- src/UE/Application/Ports/TimerPort.cpp
+++ src/UE/Application/Ports/TimerPort.cpp
@@ -1,11 +1,14 @@
 #include "TimerPort.hpp"
+#include <thread>
+#include <chrono>
 
 namespace ue
 {
 
     TimerPort::TimerPort(common::ILogger &logger)
-        : logger(logger, "[TIMER PORT]")
-    {}
+        : logger(logger, "[TIMER PORT]"), isTimerActive(false)
+    {
+    }
 
     void TimerPort::start(ITimerEventsHandler &handler)
     {
@@ -16,17 +19,38 @@ namespace ue
     void TimerPort::stop()
     {
         logger.logDebug("Stoped");
+        stopTimer();
         handler = nullptr;
     }
 
     void TimerPort::startTimer(Duration duration)
     {
         logger.logDebug("Start timer: ", duration.count(), "ms");
+        stopTimer();
+
+        isTimerActive = true;
+        std::thread(&TimerPort::runTimer, this, duration).detach();
     }
 
     void TimerPort::stopTimer()
     {
         logger.logDebug("Stop timer");
+        isTimerActive = false;
     }
 
+    void TimerPort::runTimer(Duration duration)
+    {
+        std::this_thread::sleep_for(duration);
+
+        if (isTimerActive && handler != nullptr)
+        {
+            logger.logDebug("Timer expired. Calling handleTimeout.");
+            handler->handleTimeout();
+        }
+    }
+
+    TimerPort::~TimerPort()
+    {
+        stopTimer();
+    }
 }
