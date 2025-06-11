7988910d9e68b42b19bda725305f000ba75dd055
Kamil Glazer <k.glazer.kamil@interia.pl>
2025-06-11 21:27:42 +0200

    Ports for receiving calls

--- src/UE/Application/Ports/UserPort.hpp
+++ src/UE/Application/Ports/UserPort.hpp
@@ -9,8 +9,6 @@
 #include "IEventsHandler.hpp"
 #include "UeGui/ISmsComposeMode.hpp"
 #include <optional>
-#include <vector>
-#include "SmsRepository.hpp"
 
 namespace ue
 {
@@ -18,8 +16,8 @@ namespace ue
     class UserPort : public IUserPort
     {
     public:
-        UserPort(common::ILogger& logger, IUeGui& gui, common::PhoneNumber phoneNumber);
-        void start(IEventsHandler& handler);
+        UserPort(common::ILogger &logger, IUeGui &gui, common::PhoneNumber phoneNumber);
+        void start(IEventsHandler &handler);
         void stop();
 
         void showNotConnected() override;
@@ -27,26 +25,33 @@ namespace ue
         void showConnected() override;
         void showNewSms() override;
 
-        void showSmsList(const std::vector<SmsMessage>& messages);
-        void showSmsView(const SmsMessage& message);
-        void showAlert(const std::string& title, const std::string& message);
+        void showSmsList(const std::vector<SmsMessage> &messages);
+        void showSmsView(const SmsMessage &message);
+        void showAlert(const std::string &title, const std::string &message);
         void showSmsCompose() override;
+
+        void showIncomingCall(common::PhoneNumber from) override;
+        void showTalkingScreen(common::PhoneNumber peer) override;
+        void showDialMode();
 
         common::PhoneNumber getSmsRecipient() const override;
         std::string getSmsText() const override;
+
+        void appendIncomingText(const std::string &text) override;
+        void clearOutgoingText() override;
+        std::string getCallText() const override;
 
     private:
         void acceptCallback();
         void rejectCallback();
         void mailCallback();
 
-
         common::PrefixedLogger logger;
-        IUeGui& gui;
+        IUeGui &gui;
         common::PhoneNumber phoneNumber;
-        IEventsHandler* handler = nullptr;
+        IEventsHandler *handler = nullptr;
         details::GuiViewMode currentViewMode = details::VIEW_MODE_UNKNOWN;
-
+        IUeGui::ISmsComposeMode* currentSmsMode = nullptr;
     };
 
 }
