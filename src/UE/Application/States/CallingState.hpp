669a82a12f42d0601c8c3b2930ca030a42791f4a
Tomasz <gieratdoc@gmail.com>
2025-06-11 23:01:10 +0200

    Tests & IncommingCallStates

--- src/UE/Application/States/CallingState.hpp
+++ src/UE/Application/States/CallingState.hpp
@@ -17,6 +17,8 @@ namespace ue
         void handleTimeout() override;
         void handleUnknownRecipient(common::PhoneNumber peer) override;
         void handleCallReject(common::PhoneNumber peer) override;
+        void handleSmsReceived(common::PhoneNumber from, std::string text) override;Add commentMore actions
+        void handleCallRequest(common::PhoneNumber from) override;
 
     private:
         common::PhoneNumber dialedNumber;
